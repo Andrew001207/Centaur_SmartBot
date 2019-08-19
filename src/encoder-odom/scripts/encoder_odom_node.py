cd35aa5da16998e0eeddbdcc92e3ddf0fa172d97
Andrew001207 <andrey001207@gmail.com>
2019-08-19 14:07:18 +0300

    updated encoder node

--- src/encoder-odom/scripts/encoder_odom_node.py
+++ src/encoder-odom/scripts/encoder_odom_node.py
@@ -4,20 +4,22 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Vector3, Transform
 
+pos = Transform()
+
 def encoder_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + "ACK encoder info", data.data)
+    pos.translation.x += data.x
+    pos.translation.y += data.y
+    pub.publish(pos)
 
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('/odometry', anonymous=False)
+        rospy.init_node('odometry', anonymous=False)
         pub = rospy.Publisher('/odom', Transform, queue_size=10)
-        rospy.Subscriber("/sensors/encoder", String, encoder_callback)
+        rospy.Subscriber("/sensors/wheel_encoder", Vector3, encoder_callback)
         rate = rospy.Rate(10) # 10hz
         while not rospy.is_shutdown():
-            hello_str = "hello world %s" % rospy.get_time()
-            rospy.loginfo(hello_str)
-            pub.publish(hello_str)
             rate.sleep()
     except rospy.ROSInterruptException:
         pass
