8cee5f9a5e343eeb656af6f524cfca7a4d186da8
Andrew001207 <andrey001207@gmail.com>
2019-09-24 14:10:11 +0300

    Data logger created

---
+++ src/encoder-odom/scripts/lidar_logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import LaserScan
+import os
+import csv
+
+def las_callback(data):
+    rospy.loginfo(data.ranges)
+    # with open('lidar.csv', mode='w') as csv_file:
+    #     csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    #     employee_writer.writerow(['John Smith', 'Accounting', 'November'])
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('lidar_logger', anonymous=False)
+        pub = rospy.Publisher('/ad_scan', LaserScan, queue_size=10)
+        rospy.Subscriber("/scan", LaserScan, las_callback)
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
